6dba2a1c55d43c0841ef77483236234a84595090
DephPhascow <d.sinisterpsychologist@gmail.com>
2023-08-15 17:07:12 +0500

    first commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+
+setup(
+    name='py-graphql',
+    version='1.0',
+    description='Work with GraphQL',
+    author='DPhascow',
+    author_email='d.sinisterpsychologist@gmail.com',
+    url='ссылка_на_репозиторий',
+    packages=['requests'],
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.11',
+    ],
+)
