d5b1354968b9ae0341dca548f913b636db03d491
DephPhascow <d.sinisterpsychologist@gmail.com>
2023-08-15 18:10:17 +0500

    update

--- setup.py
+++ setup.py
@@ -1,16 +1,26 @@
-from setuptools import setup
+from dotenv import load_dotenv
+from os import getenv
+from setuptools import setup, find_packages
+ 
+load_dotenv()
 
+EMAIL = getenv("EMAIL")
+AUTHOR = getenv("AUTHOR")
+ 
 setup(
-    name='py-graphql',
-    version='1.0',
-    description='Work with GraphQL',
-    author='DPhascow',
-    author_email='d.sinisterpsychologist@gmail.com',
-    url='ссылка_на_репозиторий',
-    packages=['requests'],
-    classifiers=[
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 3.11',
-    ],
+    name = "py_simple_graphql",
+    version = "0.1.0",
+    keywords = ("graphql", ),
+    description = "Simple work with GraphQL",
+    long_description = "Simple work with GraphQL",
+    license = "MIT Licence",
+ 
+    url = "https://github.com/DephPhascow/py-graphql",
+    author = AUTHOR,
+    author_email = EMAIL,
+ 
+    packages = find_packages(),
+    include_package_data = True,
+    platforms = "any",
+    install_requires = ["requests"]
 )
